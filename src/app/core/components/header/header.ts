c7ea99d5148c1e88d38ce490a2b7182200e1b621
Mawele <mbizi.kevin@gmail.com>
2025-10-06 09:11:40 +0200

    create the footer and header

---
+++ src/app/core/components/header/header.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {Menubar} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+import {InputText} from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    Menubar,
+    InputText
+  ],
+  templateUrl: './header.html',
+  styleUrl: './header.scss'
+})
+export class Header implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Accueil',
+        url: '/',
+      },
+      {
+        label: 'Nos produits',
+        url: '/products',
+      },
+      {
+        label: 'A propos',
+        url: '/about',
+      },
+      {
+        label: 'Connexion',
+        url: '/login',
+      },
+    ];
+  }
+}
